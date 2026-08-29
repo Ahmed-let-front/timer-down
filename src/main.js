9116e47581f8d69dca4c50fdff38512772ad09bb
Ahmed <letcosdgp@gmail.com>
2026-08-29 09:25:04 +0300

    upload files project

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import "./css/main.css";
+const elements = {
+  DDEL: document.getElementById("count-day"),
+  HHEL: document.getElementById("count-h"),
+  MMEL: document.getElementById("count-min"),
+  SSEL: document.getElementById("count-sec"),
+  totalSeconds: 0,
+};
+const updataUi = (data) => {
+  elements.DDEL.textContent = data.DD;
+  elements.HHEL.textContent = data.HH;
+  elements.MMEL.textContent = data.MM;
+  elements.SSEL.textContent = data.SS;
+};
+const ubdateTimer = () => {
+  let DD = Math.floor(elements.totalSeconds / (3600 * 24))
+    .toString()
+    .padStart(2, "0");
+  let HH = Math.floor((elements.totalSeconds % (3600 * 24)) / 3600)
+    .toString()
+    .padStart(2, "0");
+  let MM = Math.floor((elements.totalSeconds % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  let SS = Math.floor(elements.totalSeconds % 60)
+    .toString()
+    .padStart(2, "0");
+  return { DD, HH, MM, SS };
+};
+const counter = (days) => {
+  elements.totalSeconds = days * 24 * 60 * 60;
+  updataUi(ubdateTimer());
+  const timeInterval = setInterval(() => {
+    if (elements.totalSeconds <= 0) {
+      clearInterval(timeInterval);
+      return;
+    }
+    elements.totalSeconds = Math.floor(elements.totalSeconds - 1);
+    console.log(elements.totalSeconds);
+    updataUi(ubdateTimer());
+  }, 1000);
+};
+counter(4);
